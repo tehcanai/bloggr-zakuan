dc03ea12e83bfdee7c091c3bee21e16060a8264b
arifzakuan <arifzakuanbinadnan@airasia.com>
2024-06-28 01:56:05 +0800

    fix crossorigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,11 @@ export default function RootLayout({
     >
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap"
           rel="stylesheet"
