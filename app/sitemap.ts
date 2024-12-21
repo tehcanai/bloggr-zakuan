6efcd00861a737ff8fe26c8f7a58fd07b688afcf
Arif Zakuan <arifzakuanbinadnan@airasia.com>
2024-12-21 22:02:09 +0800

    modi sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,7 @@
-import { getBlogPosts } from 'app/rant/utils'
+import { getBlogPosts } from "app/rant/utils";
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/rant/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
-
-  let routes = ['', '/rant'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
-
-  return [...routes, ...blogs]
+  return [];
 }
